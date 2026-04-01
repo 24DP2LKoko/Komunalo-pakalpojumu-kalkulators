345975a2752f84a40082007ad6223b92550e2885
24DP2LKoko <A240323LK@rvt.lv>
2026-04-01 06:44:58 +0000

    Pievienotas funkcijas nosaukumi un parbaudes "print"

--- Analīzes un salīdzināšanas.py
+++ Analīzes un salīdzināšanas.py
@@ -1,1 +1,17 @@
+def RekinuSalidzinasana():
+    print("RekinuSalidzinasana")
 
+def DatuAttelosanaGrafikos():
+    print("DatuAttelosanaGrafikos")
+
+def IzmainuAnalizePaPeriodiem():
+    print("IzmainuAnalizePaPeriodiem")
+
+def StatistikasGeneresana():
+    print("StatistikasGeneresana")
+
+def LielakoIzdevumuNoteiksana():
+    print("LielakoIzdevumuNoteiksana")
+
+def DatuEksportesana():
+    print("DatuEksportesana")
