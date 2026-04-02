c468884038be0e07c7e9d0977891e8094cfc6cf2
24DP2NKusn <a240402nk@rvt.lv>
2026-04-02 08:55:43 +0000

    Realizēta rēķinu dzēšanas sistēma

--- Nikolajs Kushnarjovs/Rekina_Dzesana.py
+++ Nikolajs Kushnarjovs/Rekina_Dzesana.py
@@ -1,2 +1,13 @@
+from Rekina_Pievienosana import pievienot_rekinu
+
 def RekinaDzesana():
-  print("RekinaDzesana")
+  print("=== Rēķina dzēšana ===")
+
+  id = int(input("Ievadiet rēķinu ID: "))
+  for rekinis in rekini:
+    if rekini[rekinis]["id"] == id:
+      del rekini[i]
+      print("Dzēst!")
+      return
+  
+  print("Nav atrasts!")
