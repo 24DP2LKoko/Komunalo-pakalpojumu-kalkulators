6fb00c02bdeaa32a08f3a4d9ae8240ece0281f15
24DP2NKusn <a240402nk@rvt.lv>
2026-04-02 08:56:56 +0000

    Realizēta rēķinu meklešanas sistēma

--- Nikolajs Kushnarjovs/Rekinu_Meklesana.py
+++ Nikolajs Kushnarjovs/Rekinu_Meklesana.py
@@ -1,2 +1,16 @@
+from Rekina_Pievienosana import pievienot_rekinu
+
 def RekinuMeklesana():
-  print("RekinuMeklesana")
+  print("=== Meklēšana ===")
+  
+  atsl = input("Ievadiet meklējamo vārdu (veids vai datums): ").lower()
+  
+  atrasti = False
+  
+  for r in rekini:
+      if atsl in r["veids"].lower() or atsl in r["datums"]:
+          print(f"{r['id']} | {r['veids']} | {r['summa']} | {r['datums']}")
+          atrasti = True
+  
+  if not atrasti:
+      print("Nekas netika atrasts.\n")
