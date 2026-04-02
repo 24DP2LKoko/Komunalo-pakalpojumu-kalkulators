059d4fbd2ab0114dab08e31904c9127648d152c4
24DP2NKusn <a240402nk@rvt.lv>
2026-04-02 08:56:20 +0000

    Realizēta rēķinu apskate sistēma

--- Nikolajs Kushnarjovs/Rekinu_Apskate.py
+++ Nikolajs Kushnarjovs/Rekinu_Apskate.py
@@ -1,2 +1,8 @@
+from Rekina_Pievienosana import pievienot_rekinu
+
 def RekinuApskate():
-  print("RekinuApskate")
+  print("ID | Veids | Summa | Datums")
+  print("-----------------------------")
+
+  for i in rekini:
+    print(f"{r['id']} | {r['veids']} | {r['summa']} | {r['datums']}")
