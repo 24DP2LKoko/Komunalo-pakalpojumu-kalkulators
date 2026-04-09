a88b5e3888b585586c121d8f96f9cd34f58c3ead
24DP2LKoko <A240323LK@rvt.lv>
2026-04-09 08:00:54 +0000

    Salabots kods lai pievienotu funkcijas no citu kodu

--- Analīzes un salīdzināšanas/Lielako_Izdevumu_Noteiksana.py
+++ Analīzes un salīdzināšanas/Lielako_Izdevumu_Noteiksana.py
@@ -1,20 +1,25 @@
+from MaksajumuAprekins import aprekinit_pakalpojumu_maksu
+
 def LielakoIzdevumuNoteiksana(rekinu_saraksts, tops=3):
     """
-    Pieņem list rekinu_saraksts un int tops, atgriež list lielako_izdevumu_saraksts.
-    Saraksts satur TOP N lielākos izdevumus ar laukiem: periods, veids, summa.
+    funkcija LielakoIzdevumuNoteiksana pieņem list tipa vērtību rekinu_saraksts un int tipa vērtību tops un atgriež list tipa vērtību lielako_izdevumu_saraksts
     """
     if not rekinu_saraksts:
         raise ValueError("Kļūda: rēķinu saraksts ir tukšs.")
 
-    visi = [
-        {
-            "periods": rekins.get("periods", "Nav norādīts"),
-            "veids": p.get("veids", "Nezināms"),
-            "summa": round(p["paterins"] * p["tarifs"], 2)
-        }
-        for rekins in rekinu_saraksts
-        for p in rekins.get("pakalpojumi", [])
-    ]
+    visi_izdevumi = []
+    for rekins in rekinu_saraksts:
+        periods = rekins.get("periods", "Nav norādīts")
+        for p in rekins.get("pakalpojumi", []):
+            veids = p.get("veids", "Nezināms")
+            maksa = round(aprekinit_pakalpojumu_maksu(p["paterins"], p["tarifs"]), 2)
+            
+            visi_izdevumi.append({
+                "periods": periods,
+                "veids": veids,
+                "summa": maksa
+            })
 
-    # Kārto dilstošā secībā un atgriež tikai norādīto skaitu
-    return sorted(visi, key=lambda x: x["summa"], reverse=True)[:tops]
+    # Kārto sarakstu dilstošā secībā un atgriež tikai doto skaitu "tops" elementu
+    lielako_izdevumu_saraksts = sorted(visi_izdevumi, key=lambda x: x["summa"], reverse=True)[:tops]
+    return lielako_izdevumu_saraksts
