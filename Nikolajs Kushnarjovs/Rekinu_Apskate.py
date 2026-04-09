d4c5523f2a39f68eb5b3252e5d93948870d9d9dc
24DP2APopm <A240367AP@rvt.lv>
2026-04-09 09:14:47 +0000

    Dzēsti liekie sistēmas faili

--- Nikolajs Kushnarjovs/Rekinu_Apskate.py
+++
@@ -1,8 +0,0 @@
-from Rekina_Pievienosana import pievienot_rekinu
-
-def RekinuApskate():
-  print("ID | Veids | Summa | Datums")
-  print("-----------------------------")
-
-  for r in rekini:
-    print(f"{r['id']} | {r['veids']} | {r['summa']} | {r['datums']}")
